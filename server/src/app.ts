b4da541ace737ca06dbc73c2c92e464f606a7231
flestnia <cendradewangga@gmail.com>
2023-02-08 22:39:09 +0700

    split project into client and server, add csp policy

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,13 +4,24 @@ import fileUpload from "express-fileupload";
 import cors from "cors";
 import morgan from "morgan";
 import dotenv from "dotenv";
+import path from "path";
+import { expressCspHeader, INLINE, NONE, SELF } from "express-csp-header";
 import cookieSession from "cookie-session";
 import Keygrip from "keygrip";
 
 import { indexRouter } from "./router";
 
 export const app = express();
-app.use(helmet()); //securing HTTP headers
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      styleSrc: ["'self'", "'unsafe-inline'"],
+      scriptSrc: ["'self'", "'unsafe-inline'"],
+      imgSrc: ["'self'", "http://res.cloudinary.com", "data:", "*"],
+    },
+  })
+); //securing HTTP headers
 
 const { ACCEPTED_URL, JWT_SECRET } = process.env;
 
@@ -19,11 +30,19 @@ dotenv.config();
 const whitelist = ACCEPTED_URL!.split(", ");
 app.use(
   cors({
-    credentials: true, //allow axios to request with "withCredentials = true"
-    origin: whitelist,
+    // credentials: true, //allow axios to request with "withCredentials = true"
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   })
 );
+
+// app.use(
+//   expressCspHeader({
+//     directives: {
+//       "img-src": [SELF],
+//     },
+//   })
+// );
 
 // const sessionKeys = COOKIE_SESSION_KEY!.split(", ");
 // const keys = new Keygrip(sessionKeys, "sha256", "hex");
@@ -38,5 +57,8 @@ app.use(
 app.use(morgan("combined")); //http request logger
 app.use(fileUpload());
 app.use(express.json());
-
+app.use(express.static(path.join(__dirname, "../public")));
 app.use(indexRouter);
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
